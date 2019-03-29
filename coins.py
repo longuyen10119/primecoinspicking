03d0724ba9c0b4ce8e1a049964ea7ed4ba5c8112
Long Nguyen <s5115614@student.ad.griffith.edu.au>
2019-03-29 18:38:38 +1000

    dynamically solved

--- coins.py
+++ coins.py
@@ -1,5 +1,7 @@
 import math
+import time
 
+### SOLVING PRIME COINS CHANGE USING DYNAMIC PROGRAMMING
 def testPrime(n):
     if n==1 or n==2:
         return False
@@ -9,9 +11,30 @@ def testPrime(n):
         if n%i ==0:
             return False
     return True
+def findCombi(amount, coins):
+    combi =  [0]*(amount+1)
+    combi[0] = 1
+
+    for c in coins:
+        for a in range(1,len(combi)):
+            if a>= c:
+                combi[a] += combi[a-c]
+    
+    return combi[amount]
+
+def generatePrimeList(n):
+    primeList = [1,2]
+    for i in range(3,n+1,2):
+        if testPrime(i):
+            primeList.append(i)
+    return primeList
 
 def main():
     n = int(input('Enter a number:'))
-    print(testPrime(n))
+    coins = generatePrimeList(n)
+    result = findCombi(n,coins)
+    print('Result is '+str(result))
+
+
 main()
     
